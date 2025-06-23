42a59253e3eb8e715515cbf5cd694bfa5a10f468
Sven Rosenzweig <sven.rosenzweig@sap.com>
2025-06-23 13:54:53 +0200

    Return correct Retry_After_Header value
    If we do not want to suspend requests (max_sleep_time = 0), the lua
    scripts, calculating the rate limit, always returns 0 for the
    Retry-After Header value. Tools like terraform/OpenTofu will not slow
    down their deployment process, but instead simply abort.
    
    With this commit the lua scripts returns the correct value rather than
    2 * max_sleep_time. Additionally we ensure never to return zero to the
    client and log a warning if the lua script calculates zero as value.
    
    This commit adds three test cases for the rate limit calculus:
    1. No limit reached
    2. Limit reached and suspension
    3. Limit reached and no suspension

--- rate_limit/lua/redis_sliding_window.lua
+++ rate_limit/lua/redis_sliding_window.lua
@@ -42,9 +42,9 @@ if (retry_after_seconds < max_sleep_time_seconds_int) and (remaining - 1 >= -max
     -- Reset expiry time for key.
     redis.call('expire', key, window_seconds_int)
     -- Return if the request can be suspended.
-    return {remaining - 1 , retry_after_seconds}
+    return {remaining - 1, retry_after_seconds}
 end
 
 -- Return if no more remaining requests and suspending request not possible.
 -- Ensure the 2nd argument (retry_after is greater than max_sleep_time_seconds_int)
-return {0, 2 * max_sleep_time_seconds_int}
+return {0, retry_after_seconds}
---
+++ rate_limit/tests/test_ratelimit_algorithm.py
@@ -0,0 +1,67 @@
+import eventlet
+import json
+import os
+import random
+import unittest
+
+from rate_limit.backend import RedisBackend
+from rate_limit.response import RateLimitExceededResponse
+from unittest.mock import MagicMock
+
+class TestRateLimitAlgorithm(unittest.TestCase):
+    def setUp(self):
+        self.configure_connection(max_sleep_time_seconds=600)
+
+    def configure_connection(self, max_sleep_time_seconds):
+        ratelimit_response = RateLimitExceededResponse(
+            status="429", status_code=429, headerlist=[], body=None, json_body=None
+        )
+        host = os.getenv("REDIS_HOST", "localhost")
+        port = os.getenv("REDIS_PORT", 6379)
+
+        self.backend = RedisBackend(host=host,
+                                    port=port,
+                                    password=None,
+                                    rate_limit_response=ratelimit_response,
+                                    max_sleep_time_seconds=max_sleep_time_seconds,
+                                    log_sleep_time_seconds=0, )
+
+        if not self.backend.is_available()[0]:
+            raise RuntimeError(f"Redis backend {host}:{port} is not available")
+
+    def test_rate_limit_hit(self):
+        rand = random.randint(1, 1000)
+        target_type = f"port-x{str(rand)}"
+
+        response = self.backend.rate_limit('local', "create", target_type, '100r/m', )
+        self.assertIsNone(response, "Expected response not to be limited")
+
+    def test_rate_limit_hit_not_suspension(self):
+        eventlet.sleep = MagicMock()
+        rand = random.randint(1, 1000)
+        target_type = f"port-y{str(rand)}"
+
+        # First request should not hit the rate limit
+        resp1 = self.backend.rate_limit('local', "suspend", target_type, '1r/m', )
+        resp2 = self.backend.rate_limit('local', "suspend", target_type, '1r/m', )
+
+        self.assertEqual(True, eventlet.sleep.called)
+        self.assertIsNone(resp1, "Expected response not to be limited")
+        self.assertIsNone(resp2, "Expected response not to be limited")
+
+    def test_rate_limit_hit_not_suspended(self):
+        #Override connection with new max sleep time
+        self.configure_connection(max_sleep_time_seconds=0)
+        rand = random.randint(1, 1000)
+        target_type = f"port-z{str(rand)}"
+
+        # First request should not hit the rate limit
+        resp1 = self.backend.rate_limit('local', "suspend", target_type, '1r/m', )
+        resp2 = self.backend.rate_limit('local', "suspend", target_type, '1r/m', )
+
+        if not resp2:
+            self.assertIsNotNone(resp2, "Expected response to be rate limited")
+
+        body = json.loads(resp2.json_body)
+        self.assertIsNone(resp1, "Expected response not to be limited")
+        self.assertEqual(body["error"]["message"], "Too Many Requests")
